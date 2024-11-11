b0ccf75c7c1b93e9e39739130f63cf2b77dd42da
Arya Bharne <127336304+arya-io@users.noreply.github.com>
2024-11-11 12:23:57 +0530

    Update home.py

--- home.py
+++ home.py
@@ -88,7 +88,7 @@ def display_home():
             """, unsafe_allow_html=True)
     
         image = Image.open("Arya.jpg")  # Load the image from the specified file path
-        st.image(image, caption="Arya", use_column_width=True, width = 320)    
+        st.image(image, caption="Arya", use_column_width=None, width = 320)    
     
         # Personal Introduction
         st.markdown("""
